1e1d6781ff48749c44cd48345d8d1c773bdb5728
Duke jojo <ysha7020@uni.sydney.edu.au>
2021-12-30 19:05:46 +1100

    update

--- src/colloc/module/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './enum'
-export * as head from './head'
-export {default as Button} from './Button'
-export {default as monitor} from './monitor'
--- src/colloc/scene/index.ts
+++
@@ -1,5 +0,0 @@
-export * as game from './game'
-export * as entry from './entry'
-export * as setting from './setting'
-export * as selector from './selector'
-export {default as preload} from './preload'
